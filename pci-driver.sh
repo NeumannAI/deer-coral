e2f0cc62a76a711cd94a8fd10ecbc0af6925e2ce
hassanbchennati <94544260+hassanbchennati@users.noreply.github.com>
2022-05-22 13:21:44 +0300

    Add files via upload

---
+++ pci-driver.sh
@@ -0,0 +1,27 @@
+sudo apt-get install curl
+
+uname -r
+
+lsmod | grep apex
+
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install gasket-dkms libedgetpu1-std
+
+sudo sh -c "echo 'SUBSYSTEM==\"apex\", MODE=\"0660\", GROUP=\"apex\"' >> /etc/udev/rules.d/65-apex.rules"
+
+sudo groupadd apex
+
+sudo adduser $USER apex
+
+lspci -nn | grep 089a
+
+ls /dev/apex_0
+
+reboot
